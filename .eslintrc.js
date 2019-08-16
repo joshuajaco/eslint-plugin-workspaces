1f1c4e02a1446bbe940ca4561d758e2d39c208c8
joshuajaco <joshua.jacobowitz@invision.de>
2019-08-16 12:48:53 +0200

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    node: 1,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
+  plugins: ['node', 'eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:eslint-plugin/all',
+  ],
+  rules: {
+    'arrow-body-style': 'error',
+    'no-use-before-define': 'error',
+    'node/prefer-global/buffer': 'error',
+    'node/prefer-global/console': 'error',
+    'node/prefer-global/process': 'error',
+    'node/prefer-global/text-decoder': 'error',
+    'node/prefer-global/url': 'error',
+    'node/prefer-global/url-search-params': 'error',
+    'eslint-plugin/require-meta-docs-url': [
+      'error',
+      {
+        pattern:
+          'https://github.com/joshuajaco/eslint-plugin-workspaces/docs/rules/{{name}}.md',
+      },
+    ],
+  },
+};
