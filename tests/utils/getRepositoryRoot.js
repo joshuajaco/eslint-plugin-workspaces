b835b2894101b147649c594dd44c8197c82a855b
Oleksii Myronenko <mironenkoalexey@gmail.com>
2021-10-19 18:01:25 +0200

    Add unit tests

--- tests/utils/getRepositoryRoot.js
+++ tests/utils/getRepositoryRoot.js
@@ -4,26 +4,55 @@ const assert = require('assert');
 const path = require('path');
 
 describe('getRepositoryRoot', () => {
-  it('should detect root path from package in monorepository', () => {
-    const pkgPath = path.resolve(
-      __dirname,
-      './mocks/repo-with-workspaces/package/',
-    );
-
-    process.chdir(pkgPath);
+  const runTest = (targetPath, expectedRoot, predefinedRoot) => {
+    process.chdir(targetPath);
 
     const utils = require('../../lib/utils');
 
-    assert.ok(utils.getRepositoryRoot() === path.resolve(pkgPath, '..'));
+    assert.ok(utils.getRepositoryRoot(predefinedRoot) === expectedRoot);
+  };
+
+  it('should detect root path from package in monorepository', () => {
+    const expectedRoot = path.resolve(
+      __dirname,
+      './mocks/repo-with-workspaces',
+    );
+    const targetPath = path.resolve(expectedRoot, './package');
+
+    runTest(targetPath, expectedRoot);
   });
 
   it('should detect root path from root in monorepository', () => {
-    const pkgPath = path.resolve(__dirname, './mocks/repo-with-workspaces/');
+    const expectedRoot = path.resolve(
+      __dirname,
+      './mocks/repo-with-workspaces/',
+    );
+    const targetPath = expectedRoot;
 
-    process.chdir(pkgPath);
+    runTest(targetPath, expectedRoot);
+  });
 
-    const utils = require('../../lib/utils');
+  it('should detect root from package in nested workspaces', () => {
+    const expectedRoot = path.resolve(
+      __dirname,
+      './mocks/repo-with-nested-workspaces/folder-two',
+    );
+    const targetPath = path.resolve(expectedRoot, './packages/package-one');
+    const lintConfig = require(path.resolve(expectedRoot, '.eslintrc'));
+    const predefinedRoot = lintConfig.settings.workspaces.root;
 
-    assert.ok(utils.getRepositoryRoot() === pkgPath);
+    runTest(targetPath, expectedRoot, predefinedRoot);
+  });
+
+  it('should detect root from root in nested workspaces', () => {
+    const expectedRoot = path.resolve(
+      __dirname,
+      './mocks/repo-with-nested-workspaces/folder-two',
+    );
+    const targetPath = expectedRoot;
+    const lintConfig = require(path.resolve(expectedRoot, '.eslintrc'));
+    const predefinedRoot = lintConfig.settings.workspaces.root;
+
+    runTest(targetPath, expectedRoot, predefinedRoot);
   });
 });
---
+++ tests/utils/mocks/repo-with-nested-workspaces/folder-two/.eslintrc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  settings: {
+    workspaces: {
+      root: __dirname,
+    },
+  },
+};
